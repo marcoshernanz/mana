95c03cc5a53249a899d40392e121f5251f1d5fa6
MarcosH911 <marcosh.craft@gmail.com>
2024-07-30 14:16:22 +0200

    Scaffolded login

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,24 @@
+export default function SignInPage() {
+  return (
+    <div>
+      <div>
+        <div>
+          <span>Login</span>
+          <span>Enter your email below to login to your account</span>
+        </div>
+        <div>
+          <span>Email</span>
+          <input />
+        </div>
+        <div>
+          <span>Password</span>
+          <input />
+        </div>
+        <div>
+          <button>Login</button>
+          <button>Login with Google</button>
+        </div>
+      </div>
+    </div>
+  );
+}
