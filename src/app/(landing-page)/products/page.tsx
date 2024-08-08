38f9ae58c06e78d3404345d284bfa0c67425cb75
natasha <natasha.hernan.p@gmail.com>
2024-08-08 20:22:33 +0200

    Created Avatar component

--- src/app/(landing-page)/products/page.tsx
+++ src/app/(landing-page)/products/page.tsx
@@ -1,3 +1,9 @@
+import Avatar from "@/components/ui/Avatar";
+
 export default function Products() {
-  return <div></div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-orange-50 dark:bg-slate-950">
+      {/* <Avatar src="/Avatar.png" alt="avatar" size="dwarf" /> */}
+    </div>
+  );
 }
---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+
+const sizes = {
+  huge: "w-52 h-52",
+  large: "w-40 h-40",
+  medium: "w-28 h-28",
+  small: "w-20 h-20",
+  dwarf: "w-10 h-10",
+};
+
+interface AvatarProps {
+  src: string;
+  href?: string;
+  size: "huge" | "large" | "medium" | "small" | "dwarf";
+  alt: string;
+  className?: string;
+}
+
+export default function Avatar({
+  href,
+  size,
+  className,
+  src,
+  alt,
+}: AvatarProps) {
+  return (
+    <div
+      className={twMerge(
+        "flex items-center justify-center",
+        className,
+        sizes[size],
+      )}
+    >
+      <img src={src} className="rounded-full"></img>
+    </div>
+  );
+}
