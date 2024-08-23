6980328b39b6d5706219e9fd48d420844890a170
MarcosH911 <marcosh.craft@gmail.com>
2024-08-23 19:25:05 +0200

    FIxed blog

--- src/app/app/blog/page.tsx
+++ src/app/app/blog/page.tsx
@@ -1,36 +1,101 @@
-// "use client";
+"use client";
 
-// import { useState } from "react";
-// import WritePost from "./writepost";
-// import BlogPost from "./blogpost";
+import { useEffect, useState } from "react";
+import WriteBlogPost from "./WriteBlogPost";
+import BlogPost from "./BlogPost";
 
-// export default function PageControll() {
-//   //change page in which you're in
-//   const [pageNumber, setPageNumber] = useState<number>(0);
+//Tags that user already add
+export type AvailableTags = {
+  tags: string[];
+};
 
-//   function SpanPage() {
-//     {
-//       // pageNumber === 0 ? <WritePost /> : <BlogPost />;
-//     }
-//   }
+export type BlogPostType = {
+  id: number;
+  title: string;
+  content: string;
+  tags: string[];
+  isRead: boolean;
+};
 
-//   function Click(direction: string) {
-//     direction === "left"
-//       ? () => setPageNumber((prevPageNumber) => prevPageNumber - 1)
-//       : setPageNumber((prevPageNumber) => prevPageNumber + 1);
+export default function BlogPage() {
+  const [blogPosts, setBlogPosts] = useState<BlogPostType[]>([]);
+  const [pageNumber, setPageNumber] = useState<number>(0);
 
-//     SpanPage;
-//   }
+  const [isLoadingData, setIsLoadingData] = useState(true);
 
-//   return (
-//     <div className="flex items-center justify-center bg-orange-50 dark:bg-slate-950">
-//       <div className="h-full w-full max-w-7xl px-10 py-28">
-//         <div className="flex h-full w-full justify-around text-white">
-//           <button onClick={() => Click("left")}>{"<"}</button>
-//           <WritePost />
-//           <button onClick={() => Click("right")}>{">"}</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  const unreadBlogPosts = blogPosts.filter((blogPost) => !blogPost.isRead);
+
+  const addBlogPost = (blogPost: BlogPostType) => {
+    setBlogPosts((prevBlogs) => [...prevBlogs, blogPost]);
+  };
+
+  const editBlogPostIsRead = (id: number, isRead: boolean) => {
+    setBlogPosts((blogPosts) => {
+      const newBlogPosts = blogPosts.map((blogPost) =>
+        id === blogPost.id ? { ...blogPost, isRead } : blogPost,
+      );
+      return newBlogPosts;
+    });
+  };
+
+  useEffect(() => {
+    if (isLoadingData) return;
+
+    window.localStorage.setItem("blog-posts", JSON.stringify(blogPosts));
+    window.localStorage.setItem("page-number", JSON.stringify(pageNumber));
+  }, [blogPosts, isLoadingData, pageNumber]);
+
+  useEffect(() => {
+    const blogPostsData: BlogPostType[] = JSON.parse(
+      window.localStorage.getItem("blog-posts") ?? "",
+    );
+    setBlogPosts(blogPostsData);
+
+    const pageNumberData: number = JSON.parse(
+      window.localStorage.getItem("page-number") ?? "0",
+    );
+    setPageNumber(pageNumberData);
+
+    setIsLoadingData(false);
+  }, []);
+
+  return (
+    <>
+      {isLoadingData ? (
+        <div className="flex h-screen w-screen items-center justify-center bg-red-600"></div>
+      ) : (
+        <div className="flex items-center justify-center bg-orange-50 dark:bg-slate-950">
+          <div className="h-full w-full max-w-7xl px-10 py-28">
+            <div className="flex h-full w-full justify-around text-white">
+              {pageNumber > 0 && (
+                <button
+                  onClick={() => setPageNumber((pageNumber) => pageNumber - 1)}
+                >
+                  {"<"}
+                </button>
+              )}
+              {pageNumber === 0 ? (
+                <WriteBlogPost addBlogPost={addBlogPost} />
+              ) : (
+                unreadBlogPosts.length > 0 && (
+                  <BlogPost
+                    blogPost={unreadBlogPosts[pageNumber - 1]}
+                    editBlogPostIsRead={editBlogPostIsRead}
+                    setPageNumber={setPageNumber}
+                  />
+                )
+              )}
+              {pageNumber < unreadBlogPosts.length && (
+                <button
+                  onClick={() => setPageNumber((pageNumber) => pageNumber + 1)}
+                >
+                  {">"}
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
