a3a430b6825fabfdea599a533fb973b76ff7a5a4
MarcosH911 <marcosh.craft@gmail.com>
2024-09-17 20:15:48 +0200

    Anarquista

---
+++ src/app/app/todo/structure/page.tsx
@@ -0,0 +1,14 @@
+// SERVER COMPONENT
+
+import Todos from "./Todos";
+
+// export type TodoType {
+//   ...
+// }
+
+export default async function TodoPage() {
+  // Get the data
+  // const session = ...
+  // const data = db.select()...
+  // return <Todos initialData={data}... />;
+}
