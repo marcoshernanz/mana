4adeb51a34c9fb17196e01d7d165189c1076f426
natasha <natasha.hernan.p@gmail.com>
2024-10-18 11:20:44 +0200

    abort merge one. Beatiful dark mode

--- src/app/app/todo/AddTodo.tsx
+++ src/app/app/todo/AddTodo.tsx
@@ -44,19 +44,19 @@ export default function AddTodo({
   };
 
   return (
-    <div className="group flex h-12 w-full items-center justify-between bg-white pl-5 focus-within:bg-blue-50/70 hover:bg-blue-50/70">
+    <div className="group flex h-12 w-full items-center justify-between bg-white pl-5 focus-within:bg-blue-50/70 hover:bg-blue-50/70 dark:bg-slate-800 dark:hover:bg-slate-700/70">
       <div className="flex h-full w-full items-center gap-2 py-1 pr-2">
         <button className="h-[1.125rem] w-[1.125rem] flex-shrink-0 rounded-full border border-blue-500 group-hover:border-blue-600"></button>
         <input
           type="text"
           placeholder="Add a task"
-          className="h-full w-full bg-transparent px-2 font-normal placeholder:text-sky-800/60 focus:outline-none focus:placeholder:text-slate-400 group-hover:placeholder:text-slate-600 focus:group-hover:placeholder:text-slate-500"
+          className="h-full w-full bg-transparent px-2 font-normal placeholder:text-sky-800/60 focus:outline-none focus:placeholder:text-slate-400 group-hover:placeholder:text-slate-600 focus:group-hover:placeholder:text-slate-500 dark:placeholder:text-sky-200/60 dark:focus:placeholder:text-slate-600 dark:group-hover:placeholder:text-slate-400 dark:focus:group-hover:placeholder:text-slate-500"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
       </div>
       <button
-        className="h-full bg-transparent px-8 font-semibold text-slate-700 transition hover:text-slate-800 hover:shadow-sm active:shadow-none group-focus-within:bg-blue-100 group-hover:bg-blue-100"
+        className="h-full bg-transparent px-8 font-semibold text-slate-700 transition hover:text-slate-800 hover:shadow-sm active:shadow-none group-focus-within:bg-blue-100 group-hover:bg-blue-100 dark:text-slate-600 dark:hover:text-slate-800 dark:group-focus-within:bg-blue-200 dark:group-hover:bg-blue-200"
         onClick={handleAddTodo}
       >
         Add
